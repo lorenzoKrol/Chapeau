a1cb9ba305692fd05729bc55e70581c0b667b7d2
lorenzoKrol <42969133+lorenzoKrol@users.noreply.github.com>
2020-06-08 16:16:01 +0200

    Add files via upload

---
+++ MenuItemService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using ChapeauModel.Models;
+using ChapeauDAL.DAOs;
+using System;
+
+namespace ChapeauLogic.Services {
+    public class MenuItemService {
+        private MenuItemDAO menuItemDAO = new MenuItemDAO();
+
+        public List<MenuItem> GetMenuItems(Category category) {
+            return menuItemDAO.GetMenuItems(category);
+        }
+
+        public void UpdateStock(MenuItem item) {
+            menuItemDAO.UpdateStock(item);
+        }
+
+        public List<MenuItem> GetAllMenuItems() {
+            return menuItemDAO.GetAllMenuItems();
+        }
+
+        public void ChangeName(MenuItem menuItem) {
+            menuItemDAO.ChangeName(menuItem);
+        }
+
+        public void ChangeStock(MenuItem menuItem) {
+            menuItemDAO.ChangeStock(menuItem);
+        }
+    }
+}
---
+++ OrderService.cs
@@ -0,0 +1,35 @@
+﻿using ChapeauModel.Models;
+using ChapeauDAL.DAOs;
+using System;
+
+namespace ChapeauLogic.Services {
+    public class OrderService {
+        private OrderDAO orderDAO = new OrderDAO();
+        private OrderItemDAO orderItemDAO = new OrderItemDAO();
+
+        public Order CreateOrder(Table table, Employee employee) {
+            return orderDAO.CreateOrder(table, employee);
+        }
+
+        public Order GetOrder(Table table) {
+            return orderDAO.GetOrder(table);
+        }
+
+        public void RemoveOrder(Order order) {
+            orderDAO.RemoveOrder(order);
+        }
+
+        private int GetOrderID(Table table) {
+            return orderDAO.GetOrderID(table);
+        }
+
+        public bool NoItems(Table table) {
+            int orderID = GetOrderID(table);
+            return orderItemDAO.NoItems(orderID);
+        }
+
+        public void AddFeedback(Order order) {
+            orderDAO.AddFeedback(order);
+        }
+    }
+}
